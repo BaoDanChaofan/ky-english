4c39092886199e0db1376fa726964dac35342b0b
赵元卓 <zhaoyuanzhuo@example.com>
2026-07-28 09:28:06 +0800

    relative paths

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ky-english/sw.js', { scope: '/ky-english/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
